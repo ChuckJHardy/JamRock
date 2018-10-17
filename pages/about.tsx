27c7962250eb7b3ef7dfbb4b3dcf0c3234dee2de
Chuck J Hardy <chuckjhardy@gmail.com>
2018-10-17 21:19:49 +0100

    Pik Now Bro

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 import Layout from '../components/Layout';
 
 export default () => (
-  <Layout title="About | Next.js + TypeScript Example">
+  <Layout title="About">
     <p>This is the about page</p>
-    <p><Link href='/'><a>Go home</a></Link></p>
+    <p><Link href='/'><a>Home</a></Link></p>
   </Layout>
 )
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 import Layout from '../components/Layout';
 
 export default () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+  <Layout title="Home">
+    <h1>Pik Space</h1>
     <p><Link href='/about'><a>About</a></Link></p>
   </Layout>
 )

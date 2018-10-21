9555248a1582dc1a085483a4099db7e43d572b45
Chuck J Hardy <chuckjhardy@gmail.com>
2018-10-21 15:22:21 +0100

    Compress Photos

--- components/Stack.tsx
+++ components/Stack.tsx
@@ -4,14 +4,14 @@ import StackGrid, { transitions, easings } from "react-stack-grid";
 const transition = transitions.scaleDown;
 
 const images = [
-  { src: './static/photos/IMG_1111.jpg', label: 'Sample image 1' },
-  { src: './static/photos/IMG_0024.jpg', label: 'Sample image 2' },
-  { src: './static/photos/IMG_0180.jpg', label: 'Sample image 4' },
-  { src: './static/photos/IMG_0200.jpg', label: 'Sample image 5' },
-  { src: './static/photos/IMG_0203.jpg', label: 'Sample image 6' },
-  { src: './static/photos/IMG_0204.jpg', label: 'Sample image 7' },
-  { src: './static/photos/IMG_0206.jpg', label: 'Sample image 8' },
-  { src: './static/photos/IMG_0207.jpg', label: 'Sample image 9' },
+  { src: require('../static/photos/IMG_1111.jpg'), label: 'Sample image 1' },
+  { src: require('../static/photos/IMG_0024.jpg'), label: 'Sample image 2' },
+  { src: require('../static/photos/IMG_0180.jpg'), label: 'Sample image 4' },
+  { src: require('../static/photos/IMG_0200.jpg'), label: 'Sample image 5' },
+  { src: require('../static/photos/IMG_0203.jpg'), label: 'Sample image 6' },
+  { src: require('../static/photos/IMG_0204.jpg'), label: 'Sample image 7' },
+  { src: require('../static/photos/IMG_0206.jpg'), label: 'Sample image 8' },
+  { src: require('../static/photos/IMG_0207.jpg'), label: 'Sample image 9' },
 ];
 
 
--- next.config.js
+++ next.config.js
@@ -1,43 +1,46 @@
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 const withTypescript = require('@zeit/next-typescript')
 const withCSS = require('@zeit/next-css')
+const withOptimizedImages = require('next-optimized-images');
 
 // fix: prevents error when .css files are required by node
 if (typeof require !== 'undefined') {
-  require.extensions['.css'] = (file) => {}
+  require.extensions['.css'] = () => {}
 }
 
 module.exports = withTypescript(
-  withCSS({
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-            publicPath: './',
-            outputPath: 'static/',
-            name: '[name].[ext]'
-          }
-        }
-      })
-
-      // https://github.com/Aerolab/nextjs-pwa
-      config.plugins.push(
-        new SWPrecacheWebpackPlugin({
-          verbose: true,
-          staticFileGlobsIgnorePatterns: [/\.next\//],
-          runtimeCaching: [
-            {
-              handler: 'networkFirst',
-              urlPattern: /^https?.*/
+  withOptimizedImages(
+    withCSS({
+      webpack(config) {
+        config.module.rules.push({
+          test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 100000,
+              publicPath: './',
+              outputPath: 'static/',
+              name: '[name].[ext]'
             }
-          ]
+          }
         })
-      )
 
-      return config
-    }
-  })
+        // https://github.com/Aerolab/nextjs-pwa
+        config.plugins.push(
+          new SWPrecacheWebpackPlugin({
+            verbose: true,
+            staticFileGlobsIgnorePatterns: [/\.next\//],
+            runtimeCaching: [
+              {
+                handler: 'networkFirst',
+                urlPattern: /^https?.*/
+              }
+            ]
+          })
+        )
+
+        return config
+      }
+    })
+  )
 )
